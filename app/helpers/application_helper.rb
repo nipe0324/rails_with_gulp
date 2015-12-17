ae88f8baf2339e3bceea24d56f0430321aa5e75a
nipe <nipe0324@gmail.com>
2015-12-18 01:33:52 +0900

    add prefix '.bundle' to css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   def gulp_asset_path(path)
-    path = REV_MANIFEST[path] if defined?(REV_MANIFEST)
-    raise 'path miss match error' if path.blank?
-    return "/assets/javascripts/#{path}" if path.end_with?('.js')
-    "/assets/stylesheets/#{path}" if path.end_with?('.css')
+    new_path = REV_MANIFEST[path] if defined?(REV_MANIFEST)
+    raise "path miss match error: #{path}" if new_path.blank?
+
+    return "/assets/javascripts/#{new_path}" if new_path.end_with?('.js')
+    "/assets/stylesheets/#{new_path}"        if new_path.end_with?('.css')
   end
 end
--- gulp/tasks/sass_scss.js
+++ gulp/tasks/sass_scss.js
@@ -1,6 +1,7 @@
 var config = require('./config');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 var rev  = require('gulp-rev');
 var minifyCss = require('gulp-minify-css');
 var notify    = require('gulp-notify');
@@ -8,6 +9,7 @@ var notify    = require('gulp-notify');
 gulp.task('compile-sass', function() {
   return gulp.src(config.stylesheet.srcSass)
       .pipe(sass({ indentedSyntax: true, errLogToConsole: true }))
+      .pipe(rename({ suffix: '.bundle' }))
       .pipe(minifyCss())
       .pipe(rev())
       .pipe(gulp.dest(config.stylesheet.dest))
@@ -19,6 +21,7 @@ gulp.task('compile-sass', function() {
 gulp.task('compile-scss', function() {
   return gulp.src(config.stylesheet.srcScss)
       .pipe(sass({ indentedSyntax: false, errLogToConsole: true }))
+      .pipe(rename({ suffix: '.bundle' }))
       .pipe(minifyCss())
       .pipe(rev())
       .pipe(gulp.dest(config.stylesheet.dest))
